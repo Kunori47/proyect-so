55e8b8fcf9537a3f70a3c09876e3d07c95de3dc1
Kunori47 <yuualph47@gmail.com>
2025-01-11 15:16:56 -0500

    creacion de archivo hecho

--- ventas.h
+++ ventas.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 
 struct Sale {
     int cantidad_lingotes;
@@ -28,17 +29,30 @@ public:
     }
 
     void generarReporte() {
+        std::ofstream archivo("facturacion.txt");
+
         std::cout << "Reporte de Ventas:\n";
+        archivo << "Reporte de Ventas:\n";
         for(const auto& venta : ventas){
             std::cout << "Cantidad: " << venta.cantidad_lingotes 
                       << ", Fecha: " << venta.fecha 
                       << ", Precio Total: $" << venta.precio_total << std::endl;
+            archivo   << "Cantidad: " << venta.cantidad_lingotes 
+                      << ", Fecha: " << venta.fecha 
+                      << ", Precio Total: $" << venta.precio_total << std::endl;
         }
         std::cout << "\nPedidos de Compra:\n";
+        archivo << "\nPedidos de Compra:\n";
         for(const auto& pedido : pedidos){
             std::cout << "Cliente: " << pedido.cliente 
                       << ", Cantidad: " << pedido.cantidad_bauxita 
                       << ", Fecha: " << pedido.fecha << std::endl;
+            archivo   << "Cliente: " << pedido.cliente 
+                      << ", Cantidad: " << pedido.cantidad_bauxita 
+                      << ", Fecha: " << pedido.fecha << std::endl;
         }
+        // Cerrar el archivo 
+        archivo.close(); 
+        std::cout << "Datos escritos en el archivo exitosamente." << std::endl;
     }
 };
